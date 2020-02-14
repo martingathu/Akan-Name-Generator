93b06e0085167427f7f6a8abb38deed76edc1dec
martingathu <martin5gathu@gmail.com>
2020-02-14 14:58:08 +0300

    create a getDay function

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function getDay(name){
+    var month = parseInt(document.getElementById("month").value);
+    var date = parseInt(document.getElementById("date").value);
+    var year = document.getElementById("year").value;
+    var century = year.slice(0, 1);
+    var years = year.slice(2, 3);
+
+    if (month === "" || month < 1 || month > 12 || date === "" || date < 0 || date > 31 || year === "" || year.length > 4 || name === ""){
+        alert("Confirm that you have entered the correct information!")
+    }else{
+        var day = Math.floor((((century/4)-2*century-1)+((5*years/4))+((26*(month+1)/10)) + date) % 7);
+        return day
+    }
+}
