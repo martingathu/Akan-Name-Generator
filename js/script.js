1082edb4adc3eaef06c2bc1dfa46278edfc5d4db
martingathu <martin5gathu@gmail.com>
2020-02-14 15:01:10 +0300

    add getName function

--- js/script.js
+++ js/script.js
@@ -24,3 +24,12 @@ function getGender(){
     }
 }
 
+function getName(){
+    var name =  document.getElementById("name").value;
+
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var day=["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday"]
+
+   
+}
